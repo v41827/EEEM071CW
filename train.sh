ea3ecf5dbdb2a510e15f30fa27e696fb88ecd4ab
YI WEN CHAN <v41827@gmail.com>
2025-03-30 16:58:23 +0100

    change lr to default 0.0003, which will be the starting ls of this run

--- train.sh
+++ train.sh
@@ -8,7 +8,7 @@ STUDENT_ID=yc01847 STUDENT_NAME="Yiwen Chan" python main.py \
 --height 224 \
 --width 224 \
 --optim amsgrad \
---lr 0.0001 \
+--lr 0.0003 \
 --max-epoch 10 \
 --lr-scheduler multi_step
 --stepsize 4 8 \
