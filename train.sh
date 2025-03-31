db4bbdab2dfdb276adc7791733eb4adc2cf947a0
YI WEN CHAN <v41827@gmail.com>
2025-03-31 10:19:17 +0100

    last two optim

--- train.sh
+++ train.sh
@@ -7,10 +7,27 @@ STUDENT_ID=yc01847 STUDENT_NAME="Yiwen Chan" python main.py \
 --root /user/HS400/yc01847/Desktop \
 --height 224 \
 --width 224 \
---optim sgd \
+--optim adam \
 --lr 0.0001 \
 --max-epoch 10 \
 --stepsize 20 40 \
 --train-batch-size 32 \
 --test-batch-size 100 \
---save-dir logs/resnet50_1e-4_32_sgd \
+--save-dir logs/resnet50_1e-4_32_adam \
+
+STUDENT_ID=yc01847 STUDENT_NAME="Yiwen Chan" python main.py \
+-s veri \
+-t veri \
+-a resnet50 \
+--root /user/HS400/yc01847/Desktop \
+--height 224 \
+--width 224 \
+--optim rmsprop \
+--lr 0.0001 \
+--max-epoch 10 \
+--stepsize 20 40 \
+--train-batch-size 32 \
+--test-batch-size 100 \
+--save-dir logs/resnet50_1e-4_32_rmsprop \
+
+
